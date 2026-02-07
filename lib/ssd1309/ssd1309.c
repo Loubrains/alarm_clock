07f6c8a6d6ffc1688e51e614bd07098cc786ecf4
Louie Atkins-Turkish <louie.atk@gmail.com>
2026-02-07 21:48:52 +0000

    Added SSD1309 driver

---
+++ lib/ssd1309/ssd1309.c
@@ -0,0 +1,393 @@
+/*
+
+MIT License
+
+Copyright (c) 2021 David Schramm
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include <pico/stdlib.h>
+#include <hardware/spi.h>
+#include <pico/binary_info.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "ssd1309.h"
+#include "font.h"
+
+inline static void swap(int32_t *a, int32_t *b)
+{
+    int32_t *t = a;
+    *a = *b;
+    *b = *t;
+}
+
+/**
+ * @brief Write a command byte to the SSD1309 display
+ *
+ * @param p Pointer to display instance
+ * @param cmd Command byte to write
+ */
+inline static void ssd1309_write_cmd(ssd1309_t *p, uint8_t cmd)
+{
+    gpio_put(p->dc_pin, 0); // DC low = command mode
+    gpio_put(p->cs_pin, 0); // CS low = select device
+    spi_write_blocking(p->spi_i, &cmd, 1);
+    gpio_put(p->cs_pin, 1); // CS high = deselect device
+}
+
+/**
+ * @brief Write data bytes to the SSD1309 display
+ *
+ * @param p Pointer to display instance
+ * @param data Pointer to data buffer
+ * @param len Number of bytes to write
+ */
+inline static void ssd1309_write_data(ssd1309_t *p, uint8_t *data, size_t len)
+{
+    gpio_put(p->dc_pin, 1); // DC high = data mode
+    gpio_put(p->cs_pin, 0); // CS low = select device
+    spi_write_blocking(p->spi_i, data, len);
+    gpio_put(p->cs_pin, 1); // CS high = deselect device
+}
+
+/**
+ * @brief Perform hardware reset of the display
+ *
+ * @param p Pointer to display instance
+ */
+inline static void ssd1309_reset(ssd1309_t *p)
+{
+    gpio_put(p->rst_pin, 1);
+    sleep_ms(1);
+    gpio_put(p->rst_pin, 0);
+    sleep_ms(10);
+    gpio_put(p->rst_pin, 1);
+    sleep_ms(10);
+}
+
+inline static void ssd1309_write(ssd1309_t *p, uint8_t val)
+{
+    ssd1309_write_cmd(p, val);
+}
+
+bool ssd1309_init(ssd1309_t *p, uint16_t width, uint16_t height,
+                  spi_inst_t *spi_instance, uint8_t cs_pin, uint8_t dc_pin, uint8_t rst_pin)
+{
+    p->width = width;
+    p->height = height;
+    p->pages = height / 8;
+
+    p->spi_i = spi_instance;
+    p->cs_pin = cs_pin;
+    p->dc_pin = dc_pin;
+    p->rst_pin = rst_pin;
+
+    // Initialize GPIO pins
+    gpio_init(cs_pin);
+    gpio_set_dir(cs_pin, GPIO_OUT);
+    gpio_put(cs_pin, 1); // Deselect by default (active low)
+
+    gpio_init(dc_pin);
+    gpio_set_dir(dc_pin, GPIO_OUT);
+    gpio_put(dc_pin, 0); // Default to command mode
+
+    gpio_init(rst_pin);
+    gpio_set_dir(rst_pin, GPIO_OUT);
+    gpio_put(rst_pin, 1); // Not in reset
+
+    p->bufsize = (p->pages) * (p->width);
+    if ((p->buffer = malloc(p->bufsize)) == NULL)
+    {
+        p->bufsize = 0;
+        return false;
+    }
+
+    // Clear buffer
+    memset(p->buffer, 0, p->bufsize);
+
+    // Perform hardware reset
+    ssd1309_reset(p);
+
+    // from https://github.com/makerportal/rpi-pico-ssd1306
+    uint8_t cmds[] = {
+        SET_DISP,
+        // timing and driving scheme
+        SET_DISP_CLK_DIV,
+        0x80,
+        SET_MUX_RATIO,
+        height - 1,
+        SET_DISP_OFFSET,
+        0x00,
+        // resolution and layout
+        SET_DISP_START_LINE,
+        // charge pump
+        SET_CHARGE_PUMP,
+        p->external_vcc ? 0x10 : 0x14,
+        SET_SEG_REMAP | 0x01,   // column addr 127 mapped to SEG0
+        SET_COM_OUT_DIR | 0x08, // scan from COM[N] to COM0
+        SET_COM_PIN_CFG,
+        width > 2 * height ? 0x02 : 0x12,
+        // display
+        SET_CONTRAST,
+        0xff,
+        SET_PRECHARGE,
+        p->external_vcc ? 0x22 : 0xF1,
+        SET_VCOM_DESEL,
+        0x30,          // or 0x40?
+        SET_ENTIRE_ON, // output follows RAM contents
+        SET_NORM_INV,  // not inverted
+        SET_DISP | 0x01,
+        // address setting
+        SET_MEM_ADDR,
+        0x00, // horizontal
+    };
+
+    for (size_t i = 0; i < sizeof(cmds); ++i)
+        ssd1309_write(p, cmds[i]);
+
+    return true;
+}
+
+inline void ssd1309_deinit(ssd1309_t *p)
+{
+    free(p->buffer);
+}
+
+inline void ssd1309_poweroff(ssd1309_t *p)
+{
+    ssd1309_write(p, SET_DISP | 0x00);
+}
+
+inline void ssd1309_poweron(ssd1309_t *p)
+{
+    ssd1309_write(p, SET_DISP | 0x01);
+}
+
+inline void ssd1309_contrast(ssd1309_t *p, uint8_t val)
+{
+    ssd1309_write(p, SET_CONTRAST);
+    ssd1309_write(p, val);
+}
+
+inline void ssd1309_invert(ssd1309_t *p, uint8_t inv)
+{
+    ssd1309_write(p, SET_NORM_INV | (inv & 1));
+}
+
+inline void ssd1309_clear(ssd1309_t *p)
+{
+    memset(p->buffer, 0, p->bufsize);
+}
+
+void ssd1309_clear_pixel(ssd1309_t *p, uint32_t x, uint32_t y)
+{
+    if (x >= p->width || y >= p->height)
+        return;
+
+    p->buffer[x + p->width * (y >> 3)] &= ~(0x1 << (y & 0x07));
+}
+
+void ssd1309_draw_pixel(ssd1309_t *p, uint32_t x, uint32_t y)
+{
+    if (x >= p->width || y >= p->height)
+        return;
+
+    p->buffer[x + p->width * (y >> 3)] |= 0x1 << (y & 0x07); // y>>3==y/8 && y&0x7==y%8
+}
+
+void ssd1309_draw_line(ssd1309_t *p, int32_t x1, int32_t y1, int32_t x2, int32_t y2)
+{
+    if (x1 > x2)
+    {
+        swap(&x1, &x2);
+        swap(&y1, &y2);
+    }
+
+    if (x1 == x2)
+    {
+        if (y1 > y2)
+            swap(&y1, &y2);
+        for (int32_t i = y1; i <= y2; ++i)
+            ssd1309_draw_pixel(p, x1, i);
+        return;
+    }
+
+    float m = (float)(y2 - y1) / (float)(x2 - x1);
+
+    for (int32_t i = x1; i <= x2; ++i)
+    {
+        float y = m * (float)(i - x1) + (float)y1;
+        ssd1309_draw_pixel(p, i, (uint32_t)y);
+    }
+}
+
+void ssd1309_clear_square(ssd1309_t *p, uint32_t x, uint32_t y, uint32_t width, uint32_t height)
+{
+    for (uint32_t i = 0; i < width; ++i)
+        for (uint32_t j = 0; j < height; ++j)
+            ssd1309_clear_pixel(p, x + i, y + j);
+}
+
+void ssd1309_draw_square(ssd1309_t *p, uint32_t x, uint32_t y, uint32_t width, uint32_t height)
+{
+    for (uint32_t i = 0; i < width; ++i)
+        for (uint32_t j = 0; j < height; ++j)
+            ssd1309_draw_pixel(p, x + i, y + j);
+}
+
+void ssd1309_draw_empty_square(ssd1309_t *p, uint32_t x, uint32_t y, uint32_t width, uint32_t height)
+{
+    ssd1309_draw_line(p, x, y, x + width, y);
+    ssd1309_draw_line(p, x, y + height, x + width, y + height);
+    ssd1309_draw_line(p, x, y, x, y + height);
+    ssd1309_draw_line(p, x + width, y, x + width, y + height);
+}
+
+void ssd1309_draw_char_with_font(ssd1309_t *p, uint32_t x, uint32_t y, uint32_t scale, const uint8_t *font, char c)
+{
+    if (c < font[3] || c > font[4])
+        return;
+
+    uint32_t parts_per_line = (font[0] >> 3) + ((font[0] & 7) > 0);
+    for (uint8_t w = 0; w < font[1]; ++w)
+    { // width
+        uint32_t pp = (c - font[3]) * font[1] * parts_per_line + w * parts_per_line + 5;
+        for (uint32_t lp = 0; lp < parts_per_line; ++lp)
+        {
+            uint8_t line = font[pp];
+
+            for (int8_t j = 0; j < 8; ++j, line >>= 1)
+            {
+                if (line & 1)
+                    ssd1309_draw_square(p, x + w * scale, y + ((lp << 3) + j) * scale, scale, scale);
+            }
+
+            ++pp;
+        }
+    }
+}
+
+void ssd1309_draw_string_with_font(ssd1309_t *p, uint32_t x, uint32_t y, uint32_t scale, const uint8_t *font, const char *s)
+{
+    for (int32_t x_n = x; *s; x_n += (font[1] + font[2]) * scale)
+    {
+        ssd1309_draw_char_with_font(p, x_n, y, scale, font, *(s++));
+    }
+}
+
+void ssd1309_draw_char(ssd1309_t *p, uint32_t x, uint32_t y, uint32_t scale, char c)
+{
+    ssd1309_draw_char_with_font(p, x, y, scale, font_8x5, c);
+}
+
+void ssd1309_draw_string(ssd1309_t *p, uint32_t x, uint32_t y, uint32_t scale, const char *s)
+{
+    ssd1309_draw_string_with_font(p, x, y, scale, font_8x5, s);
+}
+
+static inline uint32_t ssd1309_bmp_get_val(const uint8_t *data, const size_t offset, uint8_t size)
+{
+    switch (size)
+    {
+    case 1:
+        return data[offset];
+    case 2:
+        return data[offset] | (data[offset + 1] << 8);
+    case 4:
+        return data[offset] | (data[offset + 1] << 8) | (data[offset + 2] << 16) | (data[offset + 3] << 24);
+    default:
+        __builtin_unreachable();
+    }
+    __builtin_unreachable();
+}
+
+void ssd1309_bmp_show_image_with_offset(ssd1309_t *p, const uint8_t *data, const long size, uint32_t x_offset, uint32_t y_offset)
+{
+    if (size < 54) // data smaller than header
+        return;
+
+    const uint32_t bfOffBits = ssd1309_bmp_get_val(data, 10, 4);
+    const uint32_t biSize = ssd1309_bmp_get_val(data, 14, 4);
+    const uint32_t biWidth = ssd1309_bmp_get_val(data, 18, 4);
+    const int32_t biHeight = (int32_t)ssd1309_bmp_get_val(data, 22, 4);
+    const uint16_t biBitCount = (uint16_t)ssd1309_bmp_get_val(data, 28, 2);
+    const uint32_t biCompression = ssd1309_bmp_get_val(data, 30, 4);
+
+    if (biBitCount != 1) // image not monochrome
+        return;
+
+    if (biCompression != 0) // image compressed
+        return;
+
+    const int table_start = 14 + biSize;
+    uint8_t color_val = 0;
+
+    for (uint8_t i = 0; i < 2; ++i)
+    {
+        if (!((data[table_start + i * 4] << 16) | (data[table_start + i * 4 + 1] << 8) | data[table_start + i * 4 + 2]))
+        {
+            color_val = i;
+            break;
+        }
+    }
+
+    uint32_t bytes_per_line = (biWidth / 8) + (biWidth & 7 ? 1 : 0);
+    if (bytes_per_line & 3)
+        bytes_per_line = (bytes_per_line ^ (bytes_per_line & 3)) + 4;
+
+    const uint8_t *img_data = data + bfOffBits;
+
+    int32_t step = biHeight > 0 ? -1 : 1;
+    int32_t border = biHeight > 0 ? -1 : -biHeight;
+
+    for (uint32_t y = biHeight > 0 ? biHeight - 1 : 0; y != (uint32_t)border; y += step)
+    {
+        for (uint32_t x = 0; x < biWidth; ++x)
+        {
+            if (((img_data[x >> 3] >> (7 - (x & 7))) & 1) == color_val)
+                ssd1309_draw_pixel(p, x_offset + x, y_offset + y);
+        }
+        img_data += bytes_per_line;
+    }
+}
+
+inline void ssd1309_bmp_show_image(ssd1309_t *p, const uint8_t *data, const long size)
+{
+    ssd1309_bmp_show_image_with_offset(p, data, size, 0, 0);
+}
+
+void ssd1309_show(ssd1309_t *p)
+{
+    uint8_t payload[] = {SET_COL_ADDR, 0, p->width - 1, SET_PAGE_ADDR, 0, p->pages - 1};
+    if (p->width == 64)
+    {
+        payload[1] += 32;
+        payload[2] += 32;
+    }
+
+    // Send column and page address commands
+    for (size_t i = 0; i < sizeof(payload); ++i)
+        ssd1309_write(p, payload[i]);
+
+    // Write buffer data to display
+    ssd1309_write_data(p, p->buffer, p->bufsize);
+}
--- main.cpp
+++ main.cpp
@@ -4,25 +4,33 @@
 
 #include "pico/stdlib.h"
 #include "hardware/i2c.h"
+#include "hardware/spi.h"
 #include "hardware/sync.h"
 
 #include <RTClib.h>
+extern "C"
+{
+#include "ssd1309.h"
+}
 
 #define RTC_SDA_PIN 26
 #define RTC_SCL_PIN 27
 #define RTC_INT_PIN 22
 
+#define OLED_CLK_PIN 2
+#define OLED_DIN_PIN 3
+#define OLED_CS_PIN 5
+#define OLED_DC_PIN 6
+#define OLED_RST_PIN 7
+
+#define DISPLAY_WIDTH 128
+#define DISPLAY_HEIGHT 64
+
 i2c_inst_t *i2c = i2c1;
+spi_inst_t *spi = spi0;
 RTC_DS3231 rtc;
 
 DateTime DEFAULT_DATETIME = DateTime(2000, 1, 1, 0, 0, 0); // 2000-01-01 00:00:00
-
-void initGPIO()
-{
-    gpio_init(RTC_INT_PIN);
-    gpio_set_dir(RTC_INT_PIN, GPIO_IN);
-    gpio_pull_up(RTC_INT_PIN);
-}
 
 // void setupInterrupts()
 // {
@@ -42,11 +50,61 @@ void programAlarmTest()
     rtc.setAlarm1(alarm, DS3231_A1_Hour);
 }
 
+int displayTest(ssd1309_t &display)
+{
+    // Clear the display
+    ssd1309_clear(&display);
+
+    // Draw some text
+    ssd1309_draw_string(&display, 0, 0, 2, "SPI Test");
+    ssd1309_draw_string(&display, 0, 20, 1, "Hello World!");
+    ssd1309_draw_string(&display, 0, 30, 1, "Pico + SSD1309");
+
+    // Draw some shapes
+    ssd1309_draw_line(&display, 0, 50, 127, 50);
+    ssd1309_draw_empty_square(&display, 10, 52, 20, 10);
+    ssd1309_draw_square(&display, 35, 52, 20, 10);
+
+    // Update the display
+    ssd1309_show(&display);
+
+    printf("Display updated\n");
+
+    // Animation loop
+    int x = 0;
+    int direction = 1;
+
+    while (true)
+    {
+        // Clear a strip where the animation happens
+        ssd1309_clear_square(&display, 0, 40, 128, 8);
+
+        // Draw moving pixel
+        ssd1309_draw_square(&display, x, 42, 4, 4);
+
+        // Update display
+        ssd1309_show(&display);
+
+        // Move position
+        x += direction * 2;
+        if (x >= 124 || x <= 0)
+        {
+            direction *= -1;
+        }
+
+        sleep_ms(20);
+    }
+
+    // Cleanup (never reached in this example)
+    ssd1309_deinit(&display);
+
+    return 0;
+}
+
 int main()
 {
     stdio_init_all();
-
-    initGPIO();
+    sleep_ms(2000); // Wait for USB to initialize
 
     i2c_init(i2c1, 100 * 1000);                    // 100 kHz
     gpio_set_function(RTC_SDA_PIN, GPIO_FUNC_I2C); // SDA pin
@@ -54,13 +112,38 @@ int main()
     gpio_pull_up(RTC_SDA_PIN);
     gpio_pull_up(RTC_SCL_PIN);
 
-    rtc.begin(i2c1);
+    gpio_init(RTC_INT_PIN);
+    gpio_set_dir(RTC_INT_PIN, GPIO_IN);
+    gpio_pull_up(RTC_INT_PIN);
+
+    if (!rtc.begin(i2c1))
+    {
+        printf("Failed to initialize RTC!\n");
+        return -1;
+    }
     // Adjust RTC time to compile time
     // rtc.adjust(DateTime(__DATE__, __TIME__));
     if (rtc.lostPower())
     {
+        printf("RTC lost power, setting time to %04d-%02d-%02d %02d:%02d:%02d.\n",
+               DEFAULT_DATETIME.year(), DEFAULT_DATETIME.month(), DEFAULT_DATETIME.day(),
+               DEFAULT_DATETIME.hour(), DEFAULT_DATETIME.minute(), DEFAULT_DATETIME.second());
         rtc.adjust(DEFAULT_DATETIME);
     }
+
+    spi_init(spi0, 10 * 1000 * 1000); // 10 MHz
+    gpio_set_function(OLED_CLK_PIN, GPIO_FUNC_SPI);
+    gpio_set_function(OLED_DIN_PIN, GPIO_FUNC_SPI);
+
+    ssd1309_t display;
+    if (!ssd1309_init(&display, DISPLAY_WIDTH, DISPLAY_HEIGHT,
+                      spi0, OLED_CS_PIN, OLED_DC_PIN, OLED_RST_PIN))
+    {
+        printf("Display initialization failed!\n");
+        return -1;
+    }
+    ssd1309_clear(&display);
+
     programAlarmTest();
 
     while (true)
@@ -72,14 +155,17 @@ int main()
         if (now.second() != last_time.second())
         {
             last_time = now;
-            printf("%02d:%02d:%02d\n", now.hour(), now.minute(), now.second());
+            ssd1309_clear(&display);
+            char time_str[20];
+            snprintf(time_str, sizeof(time_str), "%02d:%02d:%02d", now.hour(), now.minute(), now.second());
+            ssd1309_draw_string(&display, 0, 0, 2, time_str);
+            ssd1309_show(&display);
         }
 
         if (rtc_wake)
         {
-            printf("ALARM!\n");
-            programAlarmTest();
-            rtc_wake = false;
+            ssd1309_draw_string(&display, 0, 20, 1, "ALARM!");
+            ssd1309_show(&display);
         }
 
         // setupInterrupts();
